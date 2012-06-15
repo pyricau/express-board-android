f08f535f1d2fc8369ce581f983f62b878b0e2382
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-06-15 10:49:46 +0200

    adding ActionBarSherlock for styling and AndroidAnnotations for dev speed

--- src/info/piwai/expressboard/android/JobListActivity.java
+++ src/info/piwai/expressboard/android/JobListActivity.java
@@ -1,13 +1,15 @@
 package info.piwai.expressboard.android;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class JobListActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.actionbarsherlock.app.SherlockActivity;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.main)
+public class JobListActivity extends SherlockActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
